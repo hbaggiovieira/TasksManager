4c8084e231a117d9a6e25424523bd74d018152d6
henrique <hbaggiovieira@gmail.com>
2020-10-24 23:35:35 -0300

    Task manager app for MVVM Studies

---
+++ app/src/main/java/com/example/tasks/view/TaskFormActivity.kt
@@ -0,0 +1,98 @@
+package com.example.tasks.view
+
+import android.app.DatePickerDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.DatePicker
+import androidx.lifecycle.ViewModelProvider
+import com.example.tasks.R
+import com.example.tasks.service.model.TaskModel
+import com.example.tasks.viewmodel.TaskFormViewModel
+import kotlinx.android.synthetic.main.activity_task_form.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+class TaskFormActivity : AppCompatActivity(), View.OnClickListener,
+    DatePickerDialog.OnDateSetListener {
+
+    private lateinit var mViewModel: TaskFormViewModel
+    private val mDateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+    private val mListPriorityId: MutableList<Int> = arrayListOf()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task_form)
+
+        mViewModel = ViewModelProvider(this).get(TaskFormViewModel::class.java)
+
+        // Inicializa eventos
+        listeners()
+        observe()
+
+        mViewModel.listPriorities()
+    }
+
+    override fun onClick(v: View) {
+        val id = v.id
+        if (id == R.id.button_save) {
+            handleSave()
+        } else if (id == R.id.button_date) {
+            showDatePicker()
+        }
+    }
+
+    private fun handleSave() {
+        val task = TaskModel().apply {
+            this.description = edit_description.text.toString()
+            this.complete = check_complete.isChecked
+            this.dueDate = button_date.text.toString()
+            this.priorityId = mListPriorityId[spinner_priority.selectedItemPosition]
+        }
+
+        mViewModel.save(task)
+
+    }
+
+    private fun showDatePicker() {
+
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        DatePickerDialog(this, this, year, month, day).show()
+    }
+
+    private fun observe() {
+
+        mViewModel.priorities.observe(this, androidx.lifecycle.Observer {
+
+            val list: MutableList<String> = arrayListOf()
+            for (item in it) {
+                list.add(item.description)
+                mListPriorityId.add(item.id)
+            }
+
+            val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, list)
+            spinner_priority.adapter = adapter
+
+        })
+
+    }
+
+    private fun listeners() {
+        button_save.setOnClickListener(this)
+        button_date.setOnClickListener(this)
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        val calendar = Calendar.getInstance()
+        calendar.set(year, month, dayOfMonth)
+
+        val str = mDateFormat.format(calendar.time)
+        button_date.text = str
+    }
+
+}
---
+++ app/src/main/java/com/example/tasks/viewmodel/LoginViewModel.kt
@@ -0,0 +1,74 @@
+package com.example.tasks.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.tasks.service.model.HeaderModel
+import com.example.tasks.service.constants.TaskConstants
+import com.example.tasks.service.listener.APIListener
+import com.example.tasks.service.listener.ValidationListener
+import com.example.tasks.service.repository.PersonRepository
+import com.example.tasks.service.repository.PriorityRepository
+import com.example.tasks.service.repository.local.SecurityPreferences
+import com.example.tasks.service.repository.remote.RetrofitClient
+
+class LoginViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val mPersonRepository = PersonRepository(application)
+    private val mPriorityRepository = PriorityRepository(application)
+    private val mSharedPreferences = SecurityPreferences(application)
+
+    private val mLogin = MutableLiveData<ValidationListener>()
+    var login: LiveData<ValidationListener> = mLogin
+
+    private val mLoggedUser = MutableLiveData<Boolean>()
+    var loggedUser: LiveData<Boolean> = mLoggedUser
+
+    /**
+     * Faz login usando API
+     */
+    fun doLogin(email: String, password: String) {
+        mPersonRepository.login(email, password, object : APIListener{
+            override fun onSuccess(model: HeaderModel) {
+
+                mSharedPreferences.store(TaskConstants.SHARED.TOKEN_KEY, model.token)
+                mSharedPreferences.store(TaskConstants.SHARED.PERSON_KEY, model.personKey)
+                mSharedPreferences.store(TaskConstants.SHARED.PERSON_NAME, model.name)
+
+                //Credenciais
+                RetrofitClient.addHeader(model.token, model.personKey)
+
+                mLogin.value = ValidationListener()
+            }
+
+            override fun onFailure(str: String) {
+
+                mLogin.value = ValidationListener(str)
+            }
+
+        })
+    }
+
+    /**
+     * Verifica se usuário está logado
+     */
+    fun verifyLoggedUser() {
+
+        val token = mSharedPreferences.get(TaskConstants.SHARED.TOKEN_KEY)
+        val person = mSharedPreferences.get(TaskConstants.SHARED.PERSON_KEY)
+
+        //Credenciais
+        RetrofitClient.addHeader(token, person)
+
+        val logged = (token != "" && person != "")
+
+        if (!logged) {
+            mPriorityRepository.all()
+        }
+
+        mLoggedUser.value = logged
+
+    }
+
+}
